1d52c75d7c6fbb1438b5df66983720d725915f0d
Anton Listkov <antonlistkov@gmail.com>
2024-12-21 10:11:05 +0100

    fix middleware

--- Calculator.Api/Middlewares/OperationMiddleware.cs
+++ Calculator.Api/Middlewares/OperationMiddleware.cs
@@ -8,12 +8,13 @@ public class OperationMiddleware : IMiddleware
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         var randomNumber = new Random().Next(1, 100);
-        OperationDecorator factory = (OperationDecorator) context.RequestServices.GetService(typeof(IOperationFactory))!;
-
-        if (randomNumber % 2 == 0)
-            factory.Factory = new ReverseOperationFactory();
-        else
-            factory.Factory = new OperationFactory();
+        if (context.RequestServices.GetService(typeof(IOperationFactory)) is OperationDecorator factory)
+        {
+            if (randomNumber % 2 == 0)
+                factory.Factory = new ReverseOperationFactory();
+            else
+                factory.Factory = new OperationFactory();
+        }
         
         await next(context);
     }
